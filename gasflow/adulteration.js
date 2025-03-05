3bea0ea55e0224c856f02914fa4c7e831b3da01e
Harshinimohan25 <146054944+Harshinimohan25@users.noreply.github.com>
2025-03-05 12:56:24 +0530

    Delete gasflow directory

--- gasflow/adulteration.js
+++
@@ -1,4 +0,0 @@
-// Function to redirect to a video or external link
-function redirectToVideo() {
-    window.location.href = "https://www.youtube.com";  // Redirect to a YouTube link for more videos (example)
-}
